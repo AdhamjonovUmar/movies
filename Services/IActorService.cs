e6eeb672e0285526f6223f041050fdefab5db7c0
AdhamjonovUmar <umarilmhub@gmail.com>
2022-07-19 23:16:30 +0500

    service interface

---
+++ Services/IActorService.cs
@@ -0,0 +1,13 @@
+using movies.Entities;
+
+namespace movies.Services;
+
+public interface IActorService
+{
+    Task<(bool isSuccess, Exception e, Actor actor)> CreateAsync(Actor actor);
+    Task<List<Actor>> GetAllAsync();
+    Task<Actor> GetAsync(Guid id);
+    Task<List<Actor>> GetAllAsync(string fullname);
+    Task<(bool isSuccess, Exception e, Actor actor)> DeleteAsync(Actor actor);
+    Task<bool> ExistAsync(Guid id);
+}
