4019f642148b06cbaaeffb3c304ee10e3e8afeb8
AdhamjonovUmar <umarilmhub@gmail.com>
2022-07-20 23:51:58 +0500

    some changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using movies.Data;
+using movies.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IGenreService, GenreService>();
+builder.Services.AddTransient<IActorService, ActorService>();
+builder.Services.AddTransient<IMovieService, MovieService>();
 
 var app = builder.Build();
 
